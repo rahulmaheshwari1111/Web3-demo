9ce286a1a2d0846b47faf177f056c914121c9c9a
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2022-03-12 11:59:18 +0530

    modified the waveportal contract by adding event and struct

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,12 +4,39 @@ import "hardhat/console.sol";
 
 contract WavePortal{
     uint256 totalWaves;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave{
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
+  
+  Wave[] waves; //to store the array of structs
+
+
+
     constructor(){
         console.log("yoyo I am a contract ");
     }
-    function wave()public{
+
+
+    function wave(string memory _message)public{
       totalWaves +=1;
-      console.log("this person has waved", msg.sender);
+      console.log("this person has waved and messaged ", msg.sender );
+      console.log("the message is", _message);
+
+
+      waves.push(Wave(msg.sender, _message, block.timestamp));
+
+
+      emit NewWave(msg.sender, block.timestamp, _message);
+
+    }
+
+    function getAllWaves()public view returns(Wave[] memory){
+        return waves;
     }
     
     function getTotalWaves() public view returns (uint256) {
